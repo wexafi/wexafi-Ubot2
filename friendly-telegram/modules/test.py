f827c6cbda062052c8487101839d85cdeeafb891
wexafi <wexafi69@mail.ru>
2024-11-05 22:00:04 +0300

    Update test.py

--- friendly-telegram/modules/test.py
+++ friendly-telegram/modules/test.py
@@ -37,7 +37,7 @@ class TestMod(loader.Module):
         "logs_caption": "🗞 GeekTG logs with verbosity {}",
         "suspend_invalid_time": "🚫 <b>Invalid time to suspend</b>",
         "suspended": "🥶 <b>Bot suspended for</b> <code>{}</code> <b>seconds</b>",
-        "results_ping": "🥶 <b>Ping:</b> <code>{}</code> <b>ms</b>",
+        "results_ping": "🌘 <b>Ping:</b> <code>{}</code> <b>ms</b>",
         "confidential":(
             "⚠️ <b>Log level </b><code>{}</code><b> "
             "may reveal your confidential info, be careful</b>"
